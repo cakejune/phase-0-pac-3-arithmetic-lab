94b51e515bf81e5cb3edec47503fd67581375262
cakejune <tyq444@gmail.com>
2022-08-10 13:35:17 -0400

    commit tree e164556e177770e58a2cbb7269e7dee0241afeaa

---
+++ index.js
@@ -0,0 +1,43 @@
+let number = 5;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function makeInt(string) {
+    parseInt(string, 10);
+    return string;
+}
+
+function increment(n) {
+    n++;
+    return n;
+}
+
+function decrement(n) {
+    n--;
+    return n;
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+
+console.log(add(5, 8));
+number += 3;
